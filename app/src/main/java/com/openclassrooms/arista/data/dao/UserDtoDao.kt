54ab4c0c8584bcfbc131496d85a50ecb825398dd
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-16 16:41:17 +0200

    feat(data/dao): Add User DAO interface.

---
+++ app/src/main/java/com/openclassrooms/arista/data/dao/UserDtoDao.kt
@@ -0,0 +1,49 @@
+package com.openclassrooms.arista.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.openclassrooms.arista.data.entity.UserDto
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Data Access Object (DAO) interface of the User dto.
+ *  regroup all CRUD actions for user table.
+ */
+@Dao
+interface UserDtoDao {
+
+    /**
+     * Method to INSERT a new user.
+     * @param user the user to insert.
+     * @return represents the primary key of the inserted user row (auto-gen by Room)
+     */
+    @Insert
+    suspend fun insertUser(user: UserDto): Long
+
+
+    /**
+     * Method to GET all user.
+     * @return a Flow of the users List
+     */
+    @Query("SELECT * FROM user")
+    fun getAllUser(): Flow<List<UserDto>>
+
+    /**
+     * Method to GET an user by ID.
+     * @param id the user dto id to get.
+     * @return a Flow of the user object.
+     */
+    @Query("SELECT * FROM user")
+    fun getUserById(id:Long): Flow<UserDto>
+
+
+    /**
+     * Method to DELETE a user.
+     * @param id the user dto id to delete.
+     */
+    @Query("DELETE FROM user WHERE id = :id")
+    suspend fun deleteUserById(id: Long)
+
+
+}
