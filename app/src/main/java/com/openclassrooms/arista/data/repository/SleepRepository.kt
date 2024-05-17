63f448edc6d70fa007f968411813bfaf95b985b1
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-17 17:11:28 +0200

    feat(data/repository): remove fake API use and connect Sleep DAO.

--- app/src/main/java/com/openclassrooms/arista/data/repository/SleepRepository.kt
+++ app/src/main/java/com/openclassrooms/arista/data/repository/SleepRepository.kt
@@ -1,10 +1,24 @@
 package com.openclassrooms.arista.data.repository
 
-import com.openclassrooms.arista.data.FakeApiService
+import com.openclassrooms.arista.data.dao.SleepDtoDao
 import com.openclassrooms.arista.domain.model.Sleep
+import kotlinx.coroutines.flow.first
 
-class SleepRepository(private val apiService: FakeApiService = FakeApiService()) {
+class SleepRepository(private val sleepDao: SleepDtoDao) {
 
     // Get all sleep records
-    val allSleeps: List<Sleep> get() = apiService.getAllSleeps()
+    suspend fun getAllSleep(): List<Sleep> {
+        return sleepDao.getAllSleep()
+            .first() // Collect the first emission of the Flow
+            .map { Sleep.fromDto(it)} // Convert every DTO in sleep
+    }
+
+
+    // Add a new Sleep record
+    suspend fun addSleep(sleep: Sleep) {
+        sleepDao.insertSleep(sleep.toDto())
+    }
+
+
+
 }
