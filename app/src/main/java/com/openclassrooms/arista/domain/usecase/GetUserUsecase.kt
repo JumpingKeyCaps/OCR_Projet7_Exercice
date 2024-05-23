78629120fe87b7a5cae7e9ac53a3b383024e99c2
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-23 14:25:34 +0200

    feat(model/usecase): update the execute() and add doc.

--- app/src/main/java/com/openclassrooms/arista/domain/usecase/GetUserUsecase.kt
+++ app/src/main/java/com/openclassrooms/arista/domain/usecase/GetUserUsecase.kt
@@ -2,10 +2,20 @@ package com.openclassrooms.arista.domain.usecase
 
 import com.openclassrooms.arista.data.repository.UserRepository
 import com.openclassrooms.arista.domain.model.User
+import kotlinx.coroutines.flow.firstOrNull
 import javax.inject.Inject
 
+/**
+ * Use case  to Get the User data object.
+ */
 class GetUserUsecase @Inject constructor(private val userRepository: UserRepository) {
+
+/**
+ * Method to get the current user from the User Repository.
+ *
+ * @return a User Object
+ **/
     suspend fun execute(): User? {
-        return userRepository.getUser()
+        return userRepository.getUser().firstOrNull()
     }
 }
