54d720a35cbd8d2c90d359ddadf583fa50abef57
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-23 16:20:55 +0200

    feat(data): Update the loadUserData() method to get the user id in optional parameter, The method load the default user in case of wrong id.

--- app/src/main/java/com/openclassrooms/arista/ui/user/UserDataViewModel.kt
+++ app/src/main/java/com/openclassrooms/arista/ui/user/UserDataViewModel.kt
@@ -22,17 +22,40 @@ class UserDataViewModel @Inject constructor(private val getUserUsecase: GetUserU
     val userFlow: StateFlow<User?> = _userFlow.asStateFlow()
 
     init {
-        loadUserData()
+        //Choose your flavor ...
+
+        // loadUserData()     // return the default user profile
+         loadUserData(5)   // return the custom user profile from his ID
+        // loadUserData(999)   // if wrong id, return the default user profile
+
     }
+
 
     /**
-     * Method to load the user data.
+     * Method to load the user data with an ID (or the 1st user in bdd by default).
+     * @param id the id of the user, can be void (null by default).
      */
-    private fun loadUserData() {
+    private fun loadUserData(id: Long? = null) { // id is an optional parameter
         viewModelScope.launch(Dispatchers.IO) {
-            val user = getUserUsecase.execute()
-            _userFlow.value = user
-        }
+            val flow = if (id != null) {
+                getUserUsecase.execute(id) // Use execute(id) for specific ID
+            } else {
+                getUserUsecase.execute() // Use execute() for default current user
+            }
 
+            // Get the User from the use case returned flow.
+            flow.collect { userDto ->
+                val user = userDto?.let { User.fromDto(it) }
+                //check if the returned user is null,then call himself in default mode, else update the flow.
+                if (user==null) loadUserData() else _userFlow.value = user // update the stateflow for the fragment
+            }
+        }
     }
+
+
+
+
+
+
+
 }
