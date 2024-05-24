a7967ff355618fdd51929e3ee1a69665913e5e56
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-24 21:14:19 +0200

    feat(domain/usecase):  Upgrading the use case to return a Flow of List<SleepDto> and take the user id in parameter of the method execute().

--- app/src/main/java/com/openclassrooms/arista/domain/usecase/GetAllSleepsUseCase.kt
+++ app/src/main/java/com/openclassrooms/arista/domain/usecase/GetAllSleepsUseCase.kt
@@ -1,11 +1,22 @@
 package com.openclassrooms.arista.domain.usecase
 
+import com.openclassrooms.arista.data.entity.SleepDto
 import com.openclassrooms.arista.data.repository.SleepRepository
 import com.openclassrooms.arista.domain.model.Sleep
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+/**
+ * Use case to get all the sleep records of an user.
+ */
 class GetAllSleepsUseCase @Inject constructor(private val sleepRepository: SleepRepository) {
-    suspend fun execute(): List<Sleep> {
-        return sleepRepository.getAllSleep()
+
+    /**
+     * Method to get all sleep record of the user (by id or by default).
+     * @param userId (optional) the user id
+     * @return a Flow of the list of SleepDto
+     */
+    fun execute(userId: Long): Flow<List<SleepDto>> {
+        return sleepRepository.getAllSleep(userId)
     }
 }
