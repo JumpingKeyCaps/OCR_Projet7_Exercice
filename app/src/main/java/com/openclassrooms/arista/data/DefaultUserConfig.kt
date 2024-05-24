e31d9c092249a9fe8ca9cbb0721f9021acdc8d3d
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-24 21:21:56 +0200

    feat(data):  Create a Constant value of the default UserId, to use in database creation / loading.

---
+++ app/src/main/java/com/openclassrooms/arista/data/DefaultUserConfig.kt
@@ -0,0 +1,11 @@
+package com.openclassrooms.arista.data
+
+/**
+ * Default User configuration for the database
+ */
+class DefaultUserConfig {
+    companion object {
+        /** Default user id used by the database.**/
+        const val USER_ID: Long = 5 // Change for custom value to load the corresponding profile.
+    }
+}
