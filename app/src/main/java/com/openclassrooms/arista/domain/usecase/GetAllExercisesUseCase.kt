497281efb3b2bfd18a409bff687f92ab41f87d69
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-25 18:03:55 +0200

    feat(domain/usecase): Upgrade the execute() methode to get an userId in parameter and returning a Flow of list<ExerciseDto>.

--- app/src/main/java/com/openclassrooms/arista/domain/usecase/GetAllExercisesUseCase.kt
+++ app/src/main/java/com/openclassrooms/arista/domain/usecase/GetAllExercisesUseCase.kt
@@ -1,11 +1,13 @@
 package com.openclassrooms.arista.domain.usecase
 
+import com.openclassrooms.arista.data.entity.ExerciseDto
 import com.openclassrooms.arista.data.repository.ExerciseRepository
 import com.openclassrooms.arista.domain.model.Exercise
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetAllExercisesUseCase @Inject constructor(private val exerciseRepository: ExerciseRepository) {
-    suspend fun execute(): List<Exercise> {
-        return exerciseRepository.getAllExercises()
+    fun execute(userId: Long): Flow<List<ExerciseDto>> {
+        return exerciseRepository.getAllExercises(userId)
     }
 }
