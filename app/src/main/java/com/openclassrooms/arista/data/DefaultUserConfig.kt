1a166b1d8b8ac89e3044aed61989f80dfeda270f
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-25 16:09:28 +0200

    debug(data): Change the DefaultUserConfig value to debug.

--- app/src/main/java/com/openclassrooms/arista/data/DefaultUserConfig.kt
+++ app/src/main/java/com/openclassrooms/arista/data/DefaultUserConfig.kt
@@ -6,6 +6,6 @@ package com.openclassrooms.arista.data
 class DefaultUserConfig {
     companion object {
         /** Default user id used by the database.**/
-        const val USER_ID: Long = 5 // Change for custom value to load the corresponding profile.
+        const val USER_ID: Long = 1 // Change for custom value to load the corresponding profile.
     }
 }
