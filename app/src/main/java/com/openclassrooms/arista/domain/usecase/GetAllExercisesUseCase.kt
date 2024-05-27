616f674efd38804589c96856fc7d359e7f9083c5
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-27 14:29:55 +0200

    feat(domain/usecase): Add documentation.

--- app/src/main/java/com/openclassrooms/arista/domain/usecase/GetAllExercisesUseCase.kt
+++ app/src/main/java/com/openclassrooms/arista/domain/usecase/GetAllExercisesUseCase.kt
@@ -2,11 +2,18 @@ package com.openclassrooms.arista.domain.usecase
 
 import com.openclassrooms.arista.data.entity.ExerciseDto
 import com.openclassrooms.arista.data.repository.ExerciseRepository
-import com.openclassrooms.arista.domain.model.Exercise
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+/**
+ * Use case to get all the exercise records of an user.
+ */
 class GetAllExercisesUseCase @Inject constructor(private val exerciseRepository: ExerciseRepository) {
+    /**
+     * Method to get all exercise records of the user.
+     * @param userId  the user id.
+     * @return a Flow of the list of SleepDto.
+     */
     fun execute(userId: Long): Flow<List<ExerciseDto>> {
         return exerciseRepository.getAllExercises(userId)
     }
