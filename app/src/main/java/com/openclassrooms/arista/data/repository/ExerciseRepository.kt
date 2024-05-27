6e883a3cea670ecf9de0c2c99ccad1f8e81cb81e
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-27 14:29:13 +0200

    feat(data/repository): Inject the exerciseDao in repository constructor  + Add documentation.

--- app/src/main/java/com/openclassrooms/arista/data/repository/ExerciseRepository.kt
+++ app/src/main/java/com/openclassrooms/arista/data/repository/ExerciseRepository.kt
@@ -4,25 +4,38 @@ import com.openclassrooms.arista.data.dao.ExerciseDtoDao
 import com.openclassrooms.arista.data.entity.ExerciseDto
 import com.openclassrooms.arista.domain.model.Exercise
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
+import javax.inject.Inject
 
 /**
- *
- * Repository for exercise data access.
+ * Repository for the exercises data access.
  */
-class ExerciseRepository(private val exerciseDao: ExerciseDtoDao) {
+class ExerciseRepository @Inject constructor(private val exerciseDao: ExerciseDtoDao) {
 
     // Get all exercises
+    /**
+     * Method to get all the exercises record of an user.
+     * @param userId the user id.
+     * @return Flow of the user exercises list.
+     */
     fun getAllExercises(userId: Long): Flow<List<ExerciseDto>> {
         return exerciseDao.getAllExercises(userId)
     }
 
     // Add a new exercise
+    /**
+     * Method to add an exercise record to an user.
+     * @param exercise the exercise data object to add.
+     * @param userId the user id to use.
+     */
     suspend fun addExercise(exercise: Exercise, userId: Long) {
         exerciseDao.insertExercise(exercise.toDto(userId))
     }
 
     // Delete an exercise
+    /**
+     * Method to delete an exercise record of an user (by the exercise id).
+     * @param exercise the exercise data object to delete.
+     */
     suspend fun deleteExercise(exercise: Exercise) {
         // If there is no id, you can raise an exception and catch it in the use case and viewmodel
         exercise.id?.let {
