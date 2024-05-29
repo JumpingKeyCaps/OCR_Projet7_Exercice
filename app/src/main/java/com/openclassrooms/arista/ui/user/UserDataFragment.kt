3037f8ca941f381258d0b012e571814db68b55e0
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-29 16:04:43 +0200

    feat(ui/user):  - Code cleaning.

--- app/src/main/java/com/openclassrooms/arista/ui/user/UserDataFragment.kt
+++ app/src/main/java/com/openclassrooms/arista/ui/user/UserDataFragment.kt
@@ -1,7 +1,6 @@
 package com.openclassrooms.arista.ui.user
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -38,9 +37,6 @@ class UserDataFragment : Fragment() {
                 }
             }
         }
-
-
-
 
     }
 }
