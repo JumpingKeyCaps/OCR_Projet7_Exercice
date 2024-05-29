05fc4627b0f89c62751a7d1e4e5f7db455dabc28
Samuel Lebailly <sam.meusa@gmail.com>
2024-05-29 16:26:27 +0200

    feat(data):  Create a new Callback to pre-populate the database, used by the RoomDatabase builder.

---
+++ app/src/main/java/com/openclassrooms/arista/data/DatabasePrepopulateCallback.kt
@@ -0,0 +1,89 @@
+package com.openclassrooms.arista.data
+
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+/**
+ * The Callback used to prepopulate the database.
+ */
+class DatabasePrepopulateCallback() : RoomDatabase.Callback() {
+
+    /**
+     * Overriding the onCreate Methode to prepopulate the Database on his call.
+     * @param db the database.
+     */
+    override fun onCreate(db: SupportSQLiteDatabase) {
+        super.onCreate(db)
+        //(work only if prepopulate method is called outside a coroutine)
+        prepopulateDatabase(db)
+    }
+
+     private fun prepopulateDatabase(db: SupportSQLiteDatabase){
+        //Start the database transaction
+        db.beginTransaction()
+        try {
+            // Prepopulate with a default user
+            val defaultUserId = db.insert("user",SQLiteDatabase.CONFLICT_IGNORE,
+                ContentValues().apply{
+                put("nom","Tom Default")
+                put("email","tomdefault@test.test")
+                put("password","SdRRTq7627u324ne099hqq90in")
+            })
+            // Prepopulate with a custom default user with a selected id.
+            val customUserId = db.insert("user",SQLiteDatabase.CONFLICT_IGNORE,
+                ContentValues().apply{
+                    put("nom","John Custom")
+                    put("email","CustomJhon@test.test")
+                    put("password","Fefsfdtresfe4535sdfgs0in")
+                    put("id",5)
+                })
+
+            // Prepopulate with Sleep sessions using the inserted default user ID
+            db.insert(
+                "sleep",
+                SQLiteDatabase.CONFLICT_IGNORE,
+                ContentValues().apply {
+                    put("start_time", LocalDateTime.now().minusDays(1).atZone(ZoneOffset.UTC).toInstant().toEpochMilli())
+                    put("duration", 480)
+                    put("quality", 4)
+                    put("owner_id", defaultUserId) // Use the inserted default user ID
+                }
+            )
+            db.insert(
+                "sleep",
+                SQLiteDatabase.CONFLICT_IGNORE,
+                ContentValues().apply {
+                    put("start_time", LocalDateTime.now().minusDays(1).atZone(ZoneOffset.UTC).toInstant().toEpochMilli())
+                    put("duration", 60)
+                    put("quality", 1)
+                    put("owner_id", defaultUserId) // Use the inserted default user ID
+                }
+            )
+            // Prepopulate with Sleep sessions using the inserted custom user ID
+            db.insert(
+                "sleep",
+                SQLiteDatabase.CONFLICT_IGNORE,
+                ContentValues().apply {
+                    put("start_time", LocalDateTime.now().minusDays(1).atZone(ZoneOffset.UTC).toInstant().toEpochMilli())
+                    put("duration", 60)
+                    put("quality", 1)
+                    put("owner_id", customUserId) // Use the inserted custom user ID
+                }
+            )
+            //transaction is all ok !
+            db.setTransactionSuccessful()
+
+        } catch (e: Exception) {
+            // Handle potential exceptions during pre-population
+        } finally {
+            //Finish the transaction
+            db.endTransaction()
+        }
+
+    }
+
+}
